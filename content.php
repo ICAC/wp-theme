03e7538642b4d9f24a09cca31e321f7e4daab239
Will Smith <will@toxon.co.uk>
2014-08-25 22:49:41 +0100

    Small Compat fixed

--- content.php
+++ content.php
@@ -13,7 +13,7 @@
 			<?php endif; // is_single() ?>
 			<?php if ( comments_open() ) : ?>
 				<div class="comments-link">
-					<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
+					<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'icac' ) . '</span>', __( '1 Reply', 'icac' ), __( '% Replies', 'icac' ) ); ?>
 				</div><!-- .comments-link -->
 			<?php endif; // comments_open() ?>
 		</header><!-- .entry-header -->
@@ -24,29 +24,29 @@
 		</div><!-- .entry-summary -->
 		<?php else : ?>
 		<div class="entry-content">
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'icac' ) ); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'icac' ), 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
 		<?php endif; ?>
 
 		<footer class="entry-meta">
 			<?php icac_entry_meta(); ?>
-			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link text-muted">', '</span>' ); ?>
+			<?php edit_post_link( __( 'Edit', 'icac' ), '<span class="edit-link text-muted">', '</span>' ); ?>
 			<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries. ?>
 				<div class="author-info">
 					<div class="author-avatar">
 						<?php
 						/** This filter is documented in author.php */
-						$author_bio_avatar_size = apply_filters( 'twentytwelve_author_bio_avatar_size', 68 );
+						$author_bio_avatar_size = apply_filters( 'icac_author_bio_avatar_size', 68 );
 						echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
 						?>
 					</div><!-- .author-avatar -->
 					<div class="author-description">
-						<h2><?php printf( __( 'About %s', 'twentytwelve' ), get_the_author() ); ?></h2>
+						<h2><?php printf( __( 'About %s', 'icac' ), get_the_author() ); ?></h2>
 						<p><?php the_author_meta( 'description' ); ?></p>
 						<div class="author-link">
 							<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-								<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentytwelve' ), get_the_author() ); ?>
+								<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'icac' ), get_the_author() ); ?>
 							</a>
 						</div><!-- .author-link	-->
 					</div><!-- .author-description -->
