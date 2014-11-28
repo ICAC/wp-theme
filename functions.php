b84cb3afe152c79126816b8200f601c629952a7f
Will Smith <william.smith12@imperial.ac.uk>
2014-11-28 19:21:39 +0000

    Fixed title and single

--- functions.php
+++ functions.php
@@ -106,11 +106,27 @@ function icac_widgets_init() {
 }
 add_action( 'widgets_init', 'icac_widgets_init' );
 
-function icac_wp_title( $title )
-{
-  if( empty( $title ) && ( is_home() || is_front_page() ) ) {
-    return __( 'Home', 'theme_domain' ) . ' | ' . get_bloginfo( 'description' );
-  }
-  return $title;
+function icac_wp_title( $title, $sep ) {
+	global $paged, $page;
+
+	if ( is_feed() ) {
+		return $title;
+	}
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name', 'display' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		$title = "$title $sep $site_description";
+	}
+
+	// Add a page number if necessary.
+	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'icac' ), max( $paged, $page ) );
+	}
+
+	return $title;
 }
-add_filter( 'wp_title', 'icac_wp_title' );
+add_filter( 'wp_title', 'icac_wp_title', 10, 2 );
--- single.php
+++ single.php
@@ -1,16 +1,15 @@
 <?php get_header(); ?>
 
-	<div id="primary" class="site-content col-md-8">
+	<div id="primary" flex-gt-md="75">
 		<div id="content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', get_post_format() ); ?>
 
-				<nav class="nav-single">
-					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+				<nav class="navigation" layout="row">
+					<span class="nav-previous" flex><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
+					<span class="nav-next" flex><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
 				</nav><!-- .nav-single -->
 
 				<?php comments_template( '', true ); ?>
